a0249e258fb377e78935a33b6382e7e2ac2d1070
Edd Yerburgh <edward.yerburgh@gmail.com>
2017-03-12 17:01:57 +0000

    Fix linting error in App.vue

--- src/App.vue
+++ src/App.vue
@@ -2,18 +2,21 @@
   <div id="app">
     <Actions />
     <Getters />
+    <Modules />
   </div>
 </template>
 
 <script>
 import Actions from './components/Actions';
 import Getters from './components/Getters';
+import Modules from './components/Modules';
 
 export default {
   name: 'app',
   components: {
     Actions,
     Getters,
+    Modules,
   },
 };
 </script>
